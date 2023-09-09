ed636f1da88ffb59c9bf4e0a76ea7a7966bdbdcd
Bittu <kumarbittu775@gmail.com>
2023-09-10 00:47:23 +0530

    projects

---
+++ client/src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://www.linkpicture.com/q/E-commerce.png",
+    link: "https://github.com/Bittu04567891/E-commerce-app",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/vL0Yp5C/FoodCart.png",
+    link: "https://github.com/Bittu04567891/Restaurant",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/QD8BzYB/Revanta.png",
+    link: "https://github.com/Bittu04567891/Revanta",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/Sw3S8J7/Book-Store.png",
+    link: "https://github.com/Bittu04567891/Book-Store",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/CtmDbQg/port.png",
+    link: "https://github.com/Bittu04567891/Portfolio",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/s54jq4p/first.png",
+    link: "https://github.com/Bittu04567891/Nature-Website",
+  },
+];
